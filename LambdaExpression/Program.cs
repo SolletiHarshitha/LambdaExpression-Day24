f839bd6e4d9448554f1f20dc58b8734eef230ec1
solletiharshitha <solletiharshitha@gmail.com>
2021-07-21 22:59:35 +0530

    [Harshitha] Addin Person Details

--- LambdaExpression/Program.cs
+++ LambdaExpression/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LambdaExpression
 {
@@ -11,7 +12,7 @@ namespace LambdaExpression
             AddingPersonDetails(list);
         }
         /// <summary>
-        /// UC 1 - Person Data Management
+        /// UC 1 - Adding Person Details
         /// </summary>
         /// <param name="list"></param>
         public static void AddingPersonDetails(List<Person> list)
@@ -19,12 +20,13 @@ namespace LambdaExpression
             list.Add(new Person() { SSN = 1, Name = "Ajith", Address = "Mumbai", Age = 14});
             list.Add(new Person() { SSN = 2, Name = "Satya", Address = "Pune", Age = 75 });
             list.Add(new Person() { SSN = 3, Name = "Bhuvana", Address = "Chennai", Age = 13 });
-            list.Add(new Person() { SSN = 4, Name = "Sushmitha", Address = "Bengaluru", Age = 34 });
+            list.Add(new Person() { SSN = 4, Name = "Sushmitha", Address = "Bengaluru", Age = 33 });
             list.Add(new Person() { SSN = 5, Name = "Snehitha", Address = "Chennai", Age = 60 });
-            foreach(Person person in list)
+            foreach (Person person in list)
             {
-                Console.WriteLine("SSN : {0}\tName : {1}\tAddress : {2}\tAge : {3}",person.SSN,person.Name,person.Address,person.Age);
+                Console.WriteLine("SSN : {0}\tName : {1}\tAddress : {2}\tAge : {3}", person.SSN, person.Name, person.Address, person.Age);
             }
         }
+        
     }
 }
