9f1428c3a1b5f0e069d9938a77cc80ea02714158
solletiharshitha <solletiharshitha@gmail.com>
2021-07-22 10:19:22 +0530

    [Harshitha] Add Person Details

--- LambdaExpression/Program.cs
+++ LambdaExpression/Program.cs
@@ -22,7 +22,11 @@ namespace LambdaExpression
             list.Add(new Person() { SSN = 2, Name = "Satya", Address = "Pune", Age = 75 });
             list.Add(new Person() { SSN = 3, Name = "Bhuvana", Address = "Chennai", Age = 13 });
             list.Add(new Person() { SSN = 4, Name = "Sushmitha", Address = "Bengaluru", Age = 33 });
-            list.Add(new Person() { SSN = 5, Name = "Snehitha", Address = "Chennai", Age = 60 });
+            list.Add(new Person() { SSN = 5, Name = "Snehitha", Address = "Chennai", Age = 65 });
+            Iterate(list);
+        }
+        public static void Iterate(List<Person> list)
+        {
             foreach (Person person in list)
             {
                 Console.WriteLine("SSN : {0}\tName : {1}\tAddress : {2}\tAge : {3}", person.SSN, person.Name, person.Address, person.Age);
